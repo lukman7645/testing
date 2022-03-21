dbbd15e4ba840880f9837373d4ba82157841e5ec
Lukmanul Hakim <101858888+lukman7645@users.noreply.github.com>
2022-03-21 10:06:01 +0800

    Add files via upload

---
+++ PRAK101.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <title></title>
+    </head>
+    <body>
+    
+    <h1></h1>
+    </body>
+    <?php
+
+    $nama = "Lukmanul Hakim";
+    $nim = 2010817310002;
+    echo "Hello World in PHP<br/>";
+    echo "Nama Praktikan : $nama<br/>";
+    echo "NIM : $nim";
+
+    ?>
+    </body>
+</html>
